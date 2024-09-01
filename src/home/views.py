cdb83225aaa6e8a31fce76dc7aa9d22618d54a1e
Omar Magdy <3mormagdy@gmail.com>
2024-09-01 11:53:52 +0300

    Prepare for Railway

--- src/home/views.py
+++ src/home/views.py
@@ -5,15 +5,22 @@ from visits.models import PageVisit
 
 thid_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        precent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        precent = 0
     my_title = "My Page"
     html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "precent": (page_qs.count() * 100.0) / qs.count(),
+        "precent": precent,
         "total_visit_count": qs.count()
     }
     path = request.path
@@ -22,8 +29,7 @@ def home_page_view(request, *args, **kwargs):
     return render(request, html_template, my_context)
 
 
-
-def about_page_view(request, *args, **kwargs):
+def old_home_page_view(request, *args, **kwargs):
     my_title = "My Page"
     my_context = {
         "page_title": my_title
